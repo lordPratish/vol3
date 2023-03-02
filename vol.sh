36e8b718bfc9406d8810e23f54bc4ef2610338fb
lordPratish <55389359+lordPratish@users.noreply.github.com>
2023-03-02 20:46:11 +0200

    Update vol.sh

--- vol.sh
+++ vol.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Update package lists
-sudo apt-get update
+apt-get update
 
 # Install pip
-wget https://bootstrap.pypa.io/pip/get-pip.py
-sudo python3 get-pip.py
+if ! command -v pip3 &> /dev/null; then
+    wget https://bootstrap.pypa.io/pip/get-pip.py
+    python3 get-pip.py || { echo "Could not install pip"; exit 1; }
+fi
 
 # Upgrade setuptools
 pip3 install --upgrade setuptools
 
 # Install dependencies for pycryptodome and volatility3
-sudo apt-get install python3-dev
+apt-get install -y python3-dev
 
-# Install pycryptodome
-pip3 install pycryptodome
-
-# Install distorm3
-pip3 install distorm3 
-
-# Install yara-python
-pip3 install yara-python
+# Install pycryptodome, distorm3, and yara-python
+pip3 install pycryptodome distorm3 yara-python
 
 # Clone volatility3 repository
-git clone https://github.com/volatilityfoundation/volatility3.git
-cd volatility3
+if ! command -v git &> /dev/null; then
+    echo "Git not found. Please install Git and try again"
+    exit 1
+fi
+if [ ! -d "volatility3" ]; then
+    git clone https://github.com/volatilityfoundation/volatility3.git
+fi
+cd volatility3 || { echo "Could not change to volatility3 directory"; exit 1; }
 
 # Install minimal requirements for volatility3
 pip3 install -r requirements-minimal.txt
 
 # Display volatility3 help
-python3 vol.py -h
+if [ -d "volatility3" ]; then
+    python3 vol.py -h
+else
+    echo "Could not find volatility3 directory. Please check if the directory was created correctly"
+    exit 1
+fi
